6088cfc474f77846f0ce9c6762318f8b881710ee
Uday Pawar <udaysinghpawar0302@gmail.com>
2023-07-27 19:15:12 +0530

    env import

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -21,7 +21,7 @@ export default function Chat() {
   }, []);
 
   function connectToWs() {
-    const ws = new WebSocket(process.env.WS_URL);
+    const ws = new WebSocket(import.meta.env.WS_URL);
     setWs(ws);
     ws.addEventListener("message", handleMsg);
     ws.addEventListener("close", () => {
