3a30ea57ce7afccf4805110e5e22808d8e924f52
Uday Pawar <udaysinghpawar0302@gmail.com>
2023-07-28 16:08:50 +0530

    logged in user name displayed

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -140,7 +140,25 @@ export default function Chat() {
     <div className="flex h-screen">
       <div className="flex flex-col bg-white w-1/3 xl:w-1/4 border-r border-gray-100">
         <div className="flex-grow overflow-y-scroll thin-scroll">
-          <Logo />
+          <div className="flex justify-between items-center">
+            <Logo />
+            <div className="flex gap-1 items-center text-blue-600 font-semibold mx-2 text-lg bg-gray-100 p-2 rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-full"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <div className={"w-fit md:block hidden"}>{userName}</div>
+            </div>
+          </div>
+
           {Object.keys(peopleWithoutLoggedInUser).map((userId) => (
             <Contacts
               key={userId}
